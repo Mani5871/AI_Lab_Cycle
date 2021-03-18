a0d33c4a55c7b7c178362eba345b5d9cc168d618
Mani5871 <58336312+Mani5871@users.noreply.github.com>
2021-03-18 17:59:37 +0530

    Create water_jug.cpp

---
+++ water_jug.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+map<vector<int>, bool>map1;
+int nx, ny;
+string rule;
+
+bool check(int x, int y)
+{
+    if(map1[{x, y}])
+        return true;
+    
+    return false;
+}
+
+void rules(int x, int y)
+{
+
+    nx = 5;
+    ny = y;
+    rule = "R1";
+    if(!check(5, y) && x < 5)
+        return;
+    
+    nx = 0;
+    ny = y;
+    rule = "R2";
+    if(!check(0, y) && x > 0)
+        return;
+    
+    nx = x;
+    ny = 3;
+    rule = "R3";
+    if(!check(nx, ny) && y < 3)
+        return;
+    
+    nx = x;
+    ny = 0;
+    rule = "R4";
+    if(!check(nx, ny) && y > 0)
+        return;
+    
+    nx = x + y;
+    ny = 0;
+    rule = "R5";
+    if(!check(nx, ny) && (x + y <= 5 && y > 0))
+        return;
+    
+    nx = 0;
+    ny = x + y;
+    rule = "R6";
+    if(!check(nx, ny) && (x > 0 && x + y <= 3))
+        return;
+
+    nx = 5;
+    ny = y - (5 - x);
+    rule = "R7";
+    if(!check(nx, ny) && (x + y >= 5))
+        return;
+        
+    nx = x - (3 - y);
+    y = 3;
+    rule = "R8";
+    if(!check(nx, ny) && (x + y >= 3))
+        return;
+    
+}
+
+void water_jug(int x, int y)
+{
+    map1[{x, y}] = true;
+    while(x != 4)
+    {
+        rules(x, y);
+        x = nx;
+        y = ny;
+        cout << rule <<" ";
+        cout << x <<" " << y << endl;
+        map1[{x, y}] = true;
+    }
+    
+}
+
+
+int main() {
+    
+    water_jug(0, 0);
+}
